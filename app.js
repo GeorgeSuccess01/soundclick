c6b87fd5d4c27de2b6c0f0d295f17a4252886a3d
GeorgeSuccess01 <74687043+GeorgeSuccess01@users.noreply.github.com>
2022-05-27 04:13:22 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+
+function click(){
+const click =document.getElementById("click");
+click.style.color="white";
+click.content="SOUND CLICK";
+click.background="#4c83b7";
+
+}
+
+var audio = document.getElementById("keyone");
+function keyone(){
+				audio.play()
+}
+
+
+var audio2 = document.getElementById("keytwo");
+function keytwo(){
+				audio2.play()
+}
+
+var audio3 = document.getElementById("keythree");
+function keythree(){
+				audio3.play()
+}
+
+var audio4 = document.getElementById("keyfour");
+function keyfour(){
+				audio4.play()
+}
+
+var audio5 = document.getElementById("keyfive");
+function keyfive(){
+				audio5.play()
+}
+
+var audio6 = document.getElementById("keysix");
+function keysix(){
+				audio6.play()
+}
+
+var audio7 = document.getElementById("keyseven");
+function keyseven(){
+				audio7.play()
+}
+
+var audio8 = document.getElementById("keyeight");
+function keyeight(){
+				audio8.play()
+}
+
+var audio9 = document.getElementById("keynine");
+function keynine(){
+				audio9.play()
+}
+
+
+var audio10 = document.getElementById("keyten");
+function keyten(){
+				audio10.play()
+}
+
+var audio11 = document.getElementById("keyeleven");
+function keyeleven(){
+				audio11.play()
+}
+
+var audio12 = document.getElementById("keytwelve");
+function keytwelve(){
+				audio12.play()
+}
